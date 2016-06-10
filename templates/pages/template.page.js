3a7d4ba331d8bad285453cedfc0912dfea90ebcd
Timothy Voice <timsethvoice@gmail.com>
2016-06-10 11:31:11 -0400

    Added events and onRendered (for subs) to UI element templates

--- templates/pages/template.page.js
+++ templates/pages/template.page.js
@@ -2,8 +2,18 @@ import './{{uiName}}.html'
 
 import { Meteor } from 'meteor/meteor'
 
+Template.{{capitalize uiName}}.events({
+  'click .button'(event) {
+    console.log(`${event} fired!`);
+  },
+})
+
 Template.{{capitalize uiName}}.helpers({
   method() {
     return 'hello'
   }
+})
+
+Template.{{capitalize uiName}}.onCreated(function () {
+  /*this.subscribe('your.subscription');*/
 })
