e86c79014c3f6316e0e3eaf2171eb3f06663f171
Timothy Voice <timsethvoice@gmail.com>
2016-06-11 12:14:20 -0400

    Added basic type checking for project, API, method functions Added regex checking for UI type input

--- index.js
+++ index.js
@@ -3,29 +3,45 @@
 
 const program = require('commander');
 const functions = require('./program.functions.js');
-
+const VerEx = require('verbal-expressions');
 
 program
   .version('0.0.1')
   .command('project <name>')
   .description('Scaffold a new Meteor project')
-  .action(functions.newProject);
+  .action((name) => {
+    if (typeOf name != 'string') return console.log('Name must be a string');
+    return functions.newProject(name)
+  });
 
 program
   .command('g:method <name>')
   .description('Create a new method')
-  .action(functions.newMethod);
+  .action((name) => {
+    if (typeOf name != 'string') return console.log('Name must be a string');
+    return functions.newMethod(name)
+  });
 
 program
   .command('g:api <name>')
   .description('Create a new API with methods and tests')
   .option('-T, --no-tests', 'don\'t include a test file')
-  .action(functions.newAPI);
+  .action((name) => {
+    if (typeOf name != 'string') return console.log('Name must be a string');
+    return functions.newAPI(name)
+  });
 
 program
   .command('g:ui <type>')
   .description('Create a new UI element')
   .option('--no-sass', 'don\'t generate the SASS file for this component')
-  .action(functions.newUI);
+  .action((type, options) => {
+    
+    const typeCheck = VerEx().find('component').or('layout').or('page');
+    
+    if (!typeCheck.test(type)) return console.log('UI type not recognized');
+    
+    return functions.newUI(type, options);
+  });
 
 program.parse(process.argv);
